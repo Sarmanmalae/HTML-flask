d221a7e7594a14323f87505f943cdd256213447e
Sarmanmalae <nikitin230905@gmail.com>
2022-02-15 09:33:03 +0300

    Second commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, url_for, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('//<name>')
+@app.route('/index/<name>')
+def index(name):
+    n = name
+    return render_template('base.html', title=n)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
