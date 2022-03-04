586875df0638b3f6c45cdda1b334796b616a90fe
Sarmanmalae <nikitin230905@gmail.com>
2022-03-04 20:05:07 +0300

    Third commit

--- main.py
+++ main.py
@@ -10,5 +10,23 @@ def index(name):
     return render_template('base.html', title=n)
 
 
+@app.route('/training/<string:prof>')
+def ex2(prof):
+    for i in [i.lower() for i in prof.split()]:
+        if i in ['инженер', 'строитель']:
+            t = 'ИНЖЕНЕРНЫЕ ТРЕНАЖЕРЫ'
+        elif i in ['врач', 'ученый']:
+            t = 'НАУЧНЫЕ СИМУЛЯТОРЫ'
+        else:
+            t = 'ТРЕНАЖЕРЫ ДЛЯ ОСТАЛЬНЫХ'
+    print(t)
+    return render_template('ex2.html', title=t)
+
+
+@app.route('/test')
+def test():
+    return render_template('ex2.html', username='Kirill', title='Марс')
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
